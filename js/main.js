e6ea7d9f211eb42eed4a0d95719268e84b7ce485
Varun Jain <varunvj181@gmail.com>
2020-07-19 22:06:30 +0530

    Added icon to title

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-/**
- * Template Name: Delicious - v2.1.0
- * Template URL: https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/
- * Author: BootstrapMade.com
- * License: https://bootstrapmade.com/license/
- */
 !(function($) {
   "use strict";
 
@@ -165,55 +159,6 @@
     });
   }
 
-  // Intro carousel
-  // var heroCarousel = $("#heroCarousel");
-  // var heroCarouselIndicators = $("#hero-carousel-indicators");
-  // heroCarousel
-  //   .find(".carousel-inner")
-  //   .children(".carousel-item")
-  //   .each(function(index) {
-  //     index === 0
-  //       ? heroCarouselIndicators.append(
-  //           "<li data-target='#heroCarousel' data-slide-to='" +
-  //             index +
-  //             "' class='active'></li>"
-  //         )
-  //       : heroCarouselIndicators.append(
-  //           "<li data-target='#heroCarousel' data-slide-to='" +
-  //             index +
-  //             "'></li>"
-  //         );
-  //   });
-
-  // heroCarousel.on("slid.bs.carousel", function(e) {
-  //   $(this)
-  //     .find("h2")
-  //     .addClass("animate__animated animate__fadeInDown");
-  //   $(this)
-  //     .find("p, .btn-menu, .btn-book")
-  //     .addClass("animate__animated animate__fadeInUp");
-  // });
-
-  // Back to top button
-  // $(window).scroll(function() {
-  //   if ($(this).scrollTop() > 100) {
-  //     $(".back-to-top").fadeIn("slow");
-  //   } else {
-  //     $(".back-to-top").fadeOut("slow");
-  //   }
-  // });
-
-  // $(".back-to-top").click(function() {
-  //   $("html, body").animate(
-  //     {
-  //       scrollTop: 0
-  //     },
-  //     1500,
-  //     "easeInOutExpo"
-  //   );
-  //   return false;
-  // });
-
   // Menu list isotope and filter
   $(window).on("load", function() {
     var menuIsotope = $(".menu-container").isotope({
@@ -230,22 +175,6 @@
       });
     });
   });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  // $(".events-carousel").owlCarousel({
-  //   autoplay: true,
-  //   dots: true,
-  //   loop: true,
-  //   items: 1
-  // });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  // $(".testimonials-carousel").owlCarousel({
-  //   autoplay: true,
-  //   dots: true,
-  //   loop: true,
-  //   items: 1
-  // });
 
   // Initiate venobox lightbox
   $(document).ready(function() {
